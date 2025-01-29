5f57bbb44742459824d914a60044b3a7b6330359
SiMista <simeondeiva@gmail.com>
2025-01-29 09:29:46 +0100

    Tests added

--- src/main/java/fr/efrei/rag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/rag/web/rest/SampleResource.java
@@ -1,16 +1,13 @@
 package fr.efrei.rag.web.rest;
-
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class SampleResource {
 
-    @GetMapping("/sample/{value}")
-    public String hello(@PathVariable(value = "value", required = false) final String value){
+    @GetMapping("/samples/{value}")
+    public String hello(@PathVariable(value = "value") String value) {
         return "Hello " + value + "!";
     }
 }
--- src/test/java/fr/efrei/rag/RagApplicationTests.java
+++ src/test/java/fr/efrei/rag/RagApplicationTests.java
@@ -1,5 +1,7 @@
 package fr.efrei.rag;
 
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
